57fb78bc58be127beae2b69828597836b43d75af
kyuhmi <kyunghomin@cpp.edu>
2022-03-10 14:47:43 -0800

    started the driver code for using the postfix

---
+++ 2400Project2/src/Postfix/DriverPostfix.java
@@ -0,0 +1,14 @@
+package Postfix;
+
+public class DriverPostfix {
+    public static void main(String[] args) {
+        ConvertToPostfix translator = new ConvertToPostfix();
+        String infix = "a*b/(c-a)+d*e";
+        String postfix = translator.infixToPostfix(infix);
+
+        System.out.println("Infix Expression: " + infix);
+        System.out.println("Postfix Expression: " + postfix);
+
+        //todo: postfix evaluator when arraystack is done
+    }
+}
