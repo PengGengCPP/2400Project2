537af3441f4ba7f904e8752981a246b1a12e9fe4
kyuhmi <kyunghomin@cpp.edu>
2022-03-10 22:54:35 -0800

    set up project for junit 5 tests

---
+++ tests/ResizableArrayStackTest.java
@@ -0,0 +1,58 @@
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.EmptyStackException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Stacks.ResizableArrayStack;
+
+public class ResizableArrayStackTest {
+    private ResizableArrayStack<Integer> stack;
+
+    @BeforeEach
+    void init() {
+        stack = new ResizableArrayStack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+    }
+
+    @Test
+    void testClear() {
+        stack.clear();
+        assertThrows(EmptyStackException.class, () -> stack.peek());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertEquals(false, stack.isEmpty());
+        stack.clear();
+        assertEquals(true, stack.isEmpty());
+    }
+
+    @Test
+    void testPeek() {
+        assertEquals(3, stack.peek());
+        stack.pop();
+        assertEquals(2, stack.peek());
+        stack.pop();
+        assertEquals(1, stack.peek());
+    }
+
+    @Test
+    void testPop() {
+        assertEquals(3, stack.pop());
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+    }
+
+    @Test
+    void testPush() {
+        stack.push(1);
+        assertEquals(1, stack.peek());
+    }
+}
