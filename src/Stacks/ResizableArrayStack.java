ee9b0afb1b223b175fd09293105ec566f7266fd4
b-chen2 <chen2@cpp.edu>
2022-03-12 01:58:18 -0800

    implemented resizable array stack

--- src/Stacks/ResizableArrayStack.java
+++ src/Stacks/ResizableArrayStack.java
@@ -1,34 +1,71 @@
 package Stacks;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public class ResizableArrayStack<T> implements StackInterface<T> {
 
-    @Override
-    public void push(T entry) {
-        // TODO Auto-generated method stub
-        
-    }
+    private T[] stack;
+    private int topIndex;
 
-    @Override
-    public T pop() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public T peek() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void clear() {
-        // TODO Auto-generated method stub
-        
+    public ResizableArrayStack()
+    {
+        this(64); // the default capacity if none is given
     }
     
+    public ResizableArrayStack(int inputInitialCapacity)
+    {
+        topIndex = -1;
+        T[] stack = (T[])new Object[inputInitialCapacity];
+    }
+
+    private void ensureCapacity() // double the size if the array becomes full
+    {
+        if(topIndex >= stack.length-1)
+        {
+            stack = Arrays.copyOf(stack, stack.length*2);
+        }
+    }
+
+    @Override
+    public void push(T inputEntry)
+    {
+        ensureCapacity();
+        stack[topIndex+1] = inputEntry;
+        topIndex++;
+    }
+
+    @Override
+    public T pop()
+    {
+        T topEntry = peek();
+        stack[topIndex] = null;
+        topIndex--;
+        return topEntry;
+    }
+
+    @Override
+    public T peek()
+    {
+        if(isEmpty())
+            throw new EmptyStackException();
+        else
+            return stack[topIndex];
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return topIndex < 0;
+    }
+
+    @Override
+    public void clear()
+    {
+        while(topIndex>-1)
+        {
+            stack[topIndex] = null;
+            topIndex--;
+        }
+    }
 }
